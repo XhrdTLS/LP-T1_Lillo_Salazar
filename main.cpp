8065bcc663f362facbcff0b3d445e03dbf26e020
XhrdTLS <tomasalejandrolillosilva@gmail.com>
2021-07-08 00:19:40 -0400

    1.3
    Por problemas con el bitwise - la función pow_mult se le colocó un if en el que se intercambia el orden de los operandos, de manera que el mayor siempre quede en la segunda variable, ya que cuando el mayor estaba en la primera o 'a', el valor en binario (por lo tanto el decimal) cambiaban ligeramente, en pruebas pequeñas se comprobó que funciona.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
-//función que permite mostrar por pantalla el numero binario de un numero
+//función que permite mostrar por pantalla el numero binario de un numero (32 bits)
 void printBinary(unsigned int number){
   cout << "BIN = ";
   for (int i = 31; i >=0; --i) {
     cout << (((number >> i)%2)? '1' : '0');
   }
-  cout << " : DEC = " << number << endl;
+  cout << " : DEC = " << number;
 }
 //funcion que multiplica por potencias mayores de 2
 long pow_mult(int a, int b){
-    long pow = 0;
+    long pow = 0, temp = 0;
+    //cuando 'a' es mayor que 'b', el binario no coincide con el resultado, por lo que si es mayor, se invierte.
+    if (b < a)
+    {
+      temp = b;
+      b = a;
+      a = temp;
+    }
     //se le quita un numero, para que cuando llegue a la multiplicacion, no haga una extra.
     if (b%2==1) {
       b = b-1;
@@ -19,7 +26,7 @@ long pow_mult(int a, int b){
     while (b != 0)
     {
       if (b%2){
-          a *= b;
+          //a *= b;
           pow += a;
         }
         a = a << 1;
@@ -30,33 +37,40 @@ long pow_mult(int a, int b){
 //funcion que divide por potencias mayores de 2
 int pow_div(int a, int b){
   int pow = 0;
+  cout << b << endl;
   if (b%2==1) {
     b = b-1;
-    //cout << "es impar\n" << b << endl;
   }
   while (b != 0)
   {
     if (b % 2){
-      a /= b;
       pow += a;
+      cout << "\n a: "
+      printBinary(a);
+      cout << endl << " b:";
+      printBinary(b);
       }
-      a = a >> 1;
+      a = a << 1;
       b = b >> 1;
   }
   return pow;
 }
 void func_one(){
     //valores de prueba
-    cout << "Se comenzara con los numeros de prueba\nEn la multiplicacion seran 15 y 20\n"
-         << "el resultado es: " << pow_mult(15,20)
-         << "\n\nEn la division se tomaran los valores 350 y 15\n"
-         << "el resultado es: " << pow_div(350,15)
-         << "\n\n Ahora se podra experimentar con cualquier valor\n";
+    cout << "Se comenzara con los numeros de prueba\nEn la multiplicacion seran 20 y 15\n"
+         << "el resultado es: " << pow_mult(20, 15) << endl;
+    printBinary(pow_mult(20,15));
+    cout << "\n\nEn la division se tomaran los valores 350 y 15\n"
+         << "el resultado es: " << pow_div(350, 50) << endl;
+    printBinary(pow_div(350,50));
+    cout << "\n\n Ahora se podra experimentar con cualquier valor\n";
     int a, b;
     cout << "\n > Ingrese un numero para comenzar a multiplicar: ";
     cin >> a;
     cout << "\n > Ingrese el segundo numero: ";
     cin >> b;
+    cout << "el resultado es: " << pow_mult(a,b) << endl;
+    printBinary(pow_mult(a,b));
 
 }
 
