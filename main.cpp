034324e3251a3d5f60063d0ac4c7413e485d978d
XhrdTLS <tomasalejandrolillosilva@gmail.com>
2021-07-10 05:16:52 -0400

    1.6
    Se terminan las funciones del item 2, hay un error en el despliegue (al imprimir la matriz sumada), pero funciona correctamente lo demás

--- main.cpp
+++ main.cpp
@@ -12,80 +12,7 @@ void printBinary(unsigned int number){
   }
   cout << " : DEC = " << number << endl;
 }
-//[1.1]funcion que multiplica por potencias mayores de 2
-long pow_mult(int a, int b){
-    long pow = 0, temp = 0;
-    //cuando 'a' es mayor que 'b', el binario no coincide con el resultado, por lo que si es mayor, se invierte.
-    if (b < a)
-    {
-      temp = b;
-      b = a;
-      a = temp;
-    }
-    //se le quita un numero, para que cuando llegue a la multiplicacion, no haga una extra.
-    if (b%2==1) {
-      b = b-1;
-      //cout << "es impar\n" << b << endl;
-    }
-    while (b != 0){
-      if (b%2){
-          //a *= b;
-          pow += a;
-        }
-        a = a << 1;
-        b = b >> 1;
-    }
-    return pow;
-}
-//[1.2]funcion que divide por potencias mayores de 2
-int pow_div(int a, int b){
-  int pow = 0;
-  cout << b << endl;
-  if (b%2==1) {
-    b = b-1;
-  }
-  while (b != 0)
-  {
-    if (b % 2){
-      pow += a;
-      //cout << "\n a: ";
-      //printBinary(a);
-      //cout << endl << " b:";
-      //printBinary(b);
-      }
-      a = a >> 1;
-      b = b >> 1;
-  }
-  return pow;
-}
-//[2.1]funcion que crea una matriz
-void new_matriz(int n, int m, matriz matrizWL){ //matriz without loop
-  int i = 0, j; 
-
-  /*El loop recorre todo j antes de seguir con i ejemplo: i1,j1 > i1,j2 > i1,j3
-  de manera que avanza por filas, antes de continuar con las columnas
-  de este modo se puede usar mxn                                               */
-  loop_i: //inicio de loop en i
-    if (i < n){
-      j = 0;
-      loop_j: //inicio de loop en j
-        if (j < m){
-          cout << " > ingrese el numero que corresponde a " << i + 1 << "-" << j + 1 << ": ";
-          cin >> matrizWL[i][j];
-          j++;
-          goto loop_j;
-        }//salida del loop en j
-        i++;
-        goto loop_i;
-    }//salida del loop en i
-
-  }
-// [2.2]func
-
-// [2.3]func
-
-// [4.1]func 
-/*long new_pot(int a,int b){ // calcular potencia.
+    /*long new_pot(int a,int b){ // calcular potencia.
     int c=3;
     int i;
     int pot = c;
@@ -94,10 +21,130 @@ void new_matriz(int n, int m, matriz matrizWL){ //matriz without loop
     }
     return pot;
 } V.1*/
+//[1.1]funcion que multiplica por potencias mayores de 2
+long pow_mult(int a, int b)
+{
+  long pow = 0, temp = 0;
+  //cuando 'a' es mayor que 'b', el binario no coincide con el resultado, por lo que si es mayor, se invierte.
+  if (b < a)
+  {
+    temp = b;
+    b = a;
+    a = temp;
+  }
+  //se le quita un numero, para que cuando llegue a la multiplicacion, no haga una extra.
+  if (b % 2 == 1)
+  {
+    b = b - 1;
+    //cout << "es impar\n" << b << endl;
+  }
+  while (b != 0)
+  {
+    if (b % 2)
+    {
+      //a *= b;
+      pow += a;
+    }
+    a = a << 1;
+    b = b >> 1;
+  }
+  return pow;
+}
+//[1.2]funcion que divide por potencias mayores de 2
+int pow_div(int a, int b)
+{
+  int pow = 0;
+  //cout << b << endl;
+  if (b % 2 == 1)
+  {
+    b = b - 1;
+  }
+  while (b != 0)
+  {
+    if (b % 2)
+    {
+      pow += a;
+      //cout << "\n a: ";
+      //printBinary(a);
+      //cout << endl << " b:";
+      //printBinary(b);
+    }
+    a = a >> 1;
+    b = b >> 1;
+  }
+  return pow;
+}
 
+//[2.1]funcion que crea una matriz
+void new_matriz(int n, int m, matriz matrizWL)
+{ //matriz without loop
+  int i = 0, j;
 
-/* ------- Seccion uno ------- */
-void func_one(){
+/*El loop recorre todo j antes de seguir con i ejemplo: i1,j1 > i1,j2 > i1,j3
+  de manera que avanza por filas, antes de continuar con las columnas
+  de este modo se puede usar mxn                                               */
+loop_i: //inicio de loop en i
+  if (i < n)
+  {
+    j = 0;
+  loop_j: //inicio de loop en j
+    if (j < m)
+    {
+      cout << " > ingrese el numero que corresponde a " << i + 1 << "-" << j + 1 << ": ";
+      cin >> matrizWL[i][j];
+      j++;
+      goto loop_j;
+    } //salida del loop en j
+    i++;
+    goto loop_i;
+  } //salida del loop en i
+}
+// [2.2]funcion que genera una matriz como resultado de la suma de dos matrices
+void suma_matriz(matriz matrizA, matriz matrizB, int col, int fil)
+{
+  int i = 0, j;
+  new_matriz(col, fil, matrizB);
+loop_i: //inicio de loop en i
+  if (i < col)
+  {
+    j = 0;
+  loop_j: //inicio de loop en j
+    if (j < fil)
+    {
+      matrizB[i][j] = matrizA[i][j] + matrizB[i][j];
+      //cout << matrizB[i][j] << endl;
+      j++;
+      goto loop_j;
+    } //salida del loop en j
+    i++;
+    goto loop_i;
+  } //salida del loop en i
+}
+
+// [2.3]funcion que despliega una matriz de nxm
+void show_matriz(matriz matrizWL, int n, int m){
+  int i = 0, j;
+loop_i: //inicio de loop en i
+  if (i < n)
+  {
+    j = 0;
+  loop_j: //inicio de loop en j
+    if (j < m)
+    {
+      cout << "  " << matrizWL[i][j];
+      j++;
+      goto loop_j;
+    } //salida del loop en j
+    cout << endl;
+    i++;
+    goto loop_i;
+  } //salida del loop en i
+
+}
+
+    /* ------- Seccion uno ------------------------------------------------------------- */
+    void func_one()
+{
   int value_one, value_two, option=0;
   //valores de prueba
   cout << "Se comenzara con los numeros de prueba\nEn la multiplicacion seran 20 y 15\n"
@@ -120,19 +167,34 @@ void func_one(){
     cout << " El numero ingresado no corresponde a ninguna de las opciones." << endl;
   }
 }
-/* ------- Seccion dos ------- */
-void func_two(matriz matrizWL){
+
+/* ------- Seccion dos ------------------------------------------------------------ */
+void func_two(matriz matrizWL)
+{
   int value_n, value_m;
+  matriz matrizWL2;
   cout << "Para crear una Matriz de n x m se necesita:\n"
        << "\n > Ingresar n: ";
   cin >> value_n;
   cout << "\n > Ingresar m: ";
   cin >> value_m;
   new_matriz(value_n, value_m, matrizWL);
+  cout << "\nPara sumar matrices se utilizaran los datos de la matriz anteriormente creada\n"
+       << "y se solicitaran nuevos datos para la matriz nueva\n";
+  suma_matriz(matrizWL, matrizWL2, value_m, value_n);
+  system ("pause");
+  cout << "\nDespliege de Matrices: \n\n Matriz ingresada manualmente" << endl;
+  show_matriz(matrizWL, value_n, value_m);
+  cout << "\n Matriz calculada por la suma de dos matrices" << endl;
+  show_matriz(matrizWL2, value_n, value_m);
 }
-/* ------- Seccion tres ------- */
 
 
+// [2.3]func
+/* ------- Seccion tres ----------------------------------------------------------------- */
+
+
+/*--------- MAIN ------------------------------------------------------------------------ */
 int main(){
   matriz matriz_global;
   cout << "=========================================\n"
@@ -144,6 +206,8 @@ int main(){
   system("pause"); //solo funciona en windows
   cout << "\n2. Creacion de matrices.\n";
   func_two(matriz_global);
+
+  
   return 0;
 }
 
@@ -151,6 +215,6 @@ int main(){
    // https://www.youtube.com/watch?v=9FKV4lhXTU0&ab_channel=MateFacil (conjunto potencia)
    // base T = {x / 0 ≤ x ≤ 9} es decir P(T).
 
-void func_four(){
+/*void func_four(){
     int i, y, lim;
-}
+}*/
